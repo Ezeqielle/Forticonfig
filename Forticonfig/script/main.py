3a2cfa84adee9c3f26c4ce519639da3405ee031d
Mathis DI MASCIO <35033692+Ezeqielle@users.noreply.github.com>
2022-03-23 16:50:24 +0100

    update main.py

--- Forticonfig/script/main.py
+++ Forticonfig/script/main.py
@@ -1,5 +1,19 @@
 #!/usr/bin/python3
 
+#    ______         _   _    _____             __ _       
+#   |  ____|       | | (_)  / ____|           / _(_)      
+#   | |__ ___  _ __| |_ _  | |     ___  _ __ | |_ _  __ _ 
+#   |  __/ _ \| '__| __| | | |    / _ \| '_ \|  _| |/ _` |
+#   | | | (_) | |  | |_| | | |___| (_) | | | | | | | (_| |
+#   |_|  \___/|_|   \__|_|  \_____\___/|_| |_|_| |_|\__, |
+#                                                    __/ |
+#                                                   |___/ 
+
+# Devloppers: Ezeqielle
+# Description:
+# version: 0.1
+
+
 import paramiko
 from termcolor import colored
 import argparse
