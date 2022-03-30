06e966802f6ba28a1ca439dd33c7b5aaef619c6b
Ezeqielle <mathis.dimascio@gmail.com>
2022-03-30 10:41:07 +0200

    script:: add create dir and ssh_connect

---
+++ script/create_dir.py
@@ -0,0 +1,21 @@
+import os
+import random
+from pathlib import Path
+from datetime import date
+
+def check_dir(folder):
+    if folder.exists():
+        return True
+    else:
+        return False
+
+def create_dir():
+    disc = random.randint(1, 100000)
+    today = date.today()
+    today = today.strftime("%d%m%Y")
+    folderName = today + '_' + str(disc)
+    path = Path(__file__).parent.parent.resolve()
+    folder = path / 'output' /folderName
+    if check_dir(folder) == False:
+        os.mkdir(folder)
+    return folder
---
+++ script/ssh_connect.py
@@ -0,0 +1,35 @@
+## import modules ##
+import paramiko
+
+## check if host can be reached ##
+def ssh_check(host, user, password, port):
+    ssh = paramiko.SSHClient()
+    ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    try:
+        ssh.connect(host, username=user, password=password, port=port)
+        return True
+    except Exception as e:
+        return e
+
+## read host ##
+def read_ip():
+    with open("../config/ip.cfg") as f:
+        ip = f.read()
+    ip = ip.split("\n")
+    del ip[0]
+    return ip
+
+## read port ##
+def read_port():
+    with open("../config/port.cfg") as f:
+        port = f.read()
+    port = port.split("\n")
+    del port[0]
+    return port
+
+## read credentials ##
+def read_cred():
+    with open("../config/cred.key") as f:
+        cred = f.read()
+    cred = cred.split("\n")
+    return cred
